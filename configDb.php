28be97f0cf2b917dd34511309be2dfe328bd08d6
Soare Bianca <bianca_ellena@yahoo.com>
2018-05-09 18:17:49 +0300

    Add files via upload

---
+++ configDb.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: soarebianca
+ * Date: 27/03/2018
+ * Time: 14:04
+ */
+
+// trebuiesc adaugate datele
+$configDb = [
+    'host' => '',
+    'dbName' => '',
+    'dbUser' => '',
+    'dbPass' => '',
+];
